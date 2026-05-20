b37d9353db34adc1ec7eed023853ece64f3dedd0
Marcelle Nascimento <marcellenascim@gmail.com>
2026-05-20 13:48:09 -0300

    pronto

---
+++ produto/android/app/src/main/java/com/marcelle/produto/MainActivity.java
@@ -0,0 +1,5 @@
+package com.marcelle.produto;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ produto/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.marcelle.produto',
+  appName: 'produto',
+  webDir: 'www'
+};
+
+export default config;
